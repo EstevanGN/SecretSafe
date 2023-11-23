bad5e44a7922b8ed2df716e2d733705648d0c793
Anderson Llanos <allanosq@unal.edu.co>
2023-11-23 00:25:07 -0500

    Logo change for application

--- gui/main.py
+++ gui/main.py
@@ -709,6 +709,6 @@ class MainWindow(QMainWindow):
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("icon.ico"))
+    app.setWindowIcon(QIcon("logo.ico"))
     window = MainWindow()
     sys.exit(app.exec_())
